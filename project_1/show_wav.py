c1e582e1b0019eeef9ac29a31bfb02fd9579e6f9
Bob <chunshu_wu@alumni.brown.edu>
2018-11-11 13:58:54 -0500

    show waveform

---
+++ project_1/show_wav.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as pyplot
+import scipy.io.wavfile as wavfile
+import numpy as np
+def read_wav(filename):
+    frame_rate, sound = wavfile.read(filename)
+    if sound.ndim > 1:
+        frame_num, channel_num = sound.shape
+    else:
+        channel_num = 1
+        frame_num = sound.shape[0]
+    return sound, frame_rate, frame_num, channel_num
+filename = "./helloworld.wav"
+sound, frame_rate, frame_num, channel_num = read_wav(filename)
+if channel_num > 1:
+    sound = sound.sum(axis = 1)
+x = 1/frame_rate*np.arange(sound.size)
+pyplot.plot(x, sound/32768)
+pyplot.title('Hello world!', fontsize = 18)
+pyplot.xlabel('time (seconds)', fontsize = 14)
+pyplot.ylabel('Amplitude (max = 1)', fontsize = 14)
+print('Close plot to terminate')
+pyplot.show()
