72adc38c93af21649e058180fb9ba23700901645
Bob <chunshu_wu@alumni.brown.edu>
2018-11-11 17:53:03 -0500

    done, only to show X[n,k]

--- project_1/GFBS.py
+++ project_1/GFBS.py
@@ -1,7 +1,25 @@
 import TDFT
+import numpy as np
+from scipy import fftpack
+import matplotlib.pyplot as pyplot
 FILENAME = TDFT.FILENAME
 sound, frame_rate = TDFT.sound, TDFT.frame_rate
 frame_num, channel_num = TDFT.frame_num, TDFT.channel_num
 window_width = TDFT.window_width
 spectrum = TDFT.get_tdft(sound, window_width, frame_num)
-print(spectrum.shape)
+def get_gfbs(spectrum, window_width):
+    sound = np.array([])
+    for i in range(spectrum.shape[0]):
+        sound_in_window = fftpack.fft(spectrum[i,:])
+        sound = np.append(sound, sound_in_window)
+    sound = sound/window_width
+    return sound.real
+if __name__ == '__main__':
+    sound = get_gfbs(spectrum, window_width)
+    x = 1/frame_rate*np.arange(sound.size)
+    pyplot.plot(x, sound/32768)
+    pyplot.title('Hello world!', fontsize = 18)
+    pyplot.xlabel('time (seconds)', fontsize = 14)
+    pyplot.ylabel('Amplitude (max = 1)', fontsize = 14)
+    print('Close plot to terminate')
+    pyplot.show()
